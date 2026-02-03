9cc65a33ab999f19a8da1ca07a1f79d80e035940
reverse <mike.chevronnet@gmail.com>
2026-02-03 09:43:00 +0000

    Update: docs section - Change the cards css of the blogs in home.html

--- helpdocs/views.py
+++ helpdocs/views.py
@@ -6,20 +6,19 @@ def help_index(request):
 
 
 def ircd(request):
-    return render(request, 'help/ircd.html', {'title': 'IRCD Information', 'subtitle': 'Ircd Details'})
+    return render(request, 'help/ircd.html', {'title': 'IRCd docs', 'subtitle': 'IRC daemon administration'})
 
 
 def nickserv(request):
-    return render(request, 'help/nickserv.html', {'title': 'NickServ'})
+    return render(request, 'help/nickserv.html', {'title': 'NickServ','subtitle': 'Account management service'})
 
 
 def chanserv(request):
-    return render(request, 'help/chanserv.html', {'title': 'ChanServ'})
-
+    return render(request, 'help/chanserv.html', {'title': 'ChanServ', 'subtitle': 'Channel management service'})
 
 def memoserv(request):
-    return render(request, 'help/memoserv.html', {'title': 'MemoServ'})
+    return render(request, 'help/memoserv.html', {'title': 'MemoServ', 'subtitle': 'Memo management service'})
 
 
 def operserv(request):
-    return render(request, 'help/operserv.html', {'title': 'OperServ'})
+    return render(request, 'help/operserv.html', {'title': 'OperServ', 'subtitle': 'Operator management service'})
